eca8c0ae6dd3d5343015aa7ed06fd9fcdb562fa0
jadehoo <jadehoo@naver.com>
2022-01-19 11:26:27 +0800

    aaaa

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import lodash from "lodash";
+
+function App() {
+  useEffect(() => {});
+  return <div className="App"></div>;
+}
+
+export default App;
